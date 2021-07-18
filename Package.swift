c140d41a2b1ef36ccf9ec2958d93c58ab7058272
elppaaa <withbcd@gmail.com>
2021-07-19 02:19:00 +0900

    Add swift settings

--- Package.swift
+++ Package.swift
@@ -25,7 +25,10 @@ let package = Package(
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
     // Targets can depend on other targets in this package, and on products in packages this package depends on.
-    .target( name: "KakaoSDKCommon", dependencies: [ "Alamofire" ]),
+    .target( name: "KakaoSDKCommon",
+             dependencies: [ "Alamofire" ],
+             swiftSettings: [.define("APPLICATION_EXTENSION_API_ONLY=NO", nil)]
+           ),
     .target( name: "KakaoSDKAuth", dependencies: [ "KakaoSDKCommon", ]),
     .target( name: "KakaoSDKLink", dependencies: [ "KakaoSDKCommon", "KakaoSDKTemplate", ]),
     .target( name: "KakaoSDKNavi", dependencies: [ "KakaoSDKCommon", ]),
